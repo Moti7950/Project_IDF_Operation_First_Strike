d7519c5405c702d90c5068f0ca9c0c7f35db3feb
dov zucker <ydovzucker@gmail.com>
2025-05-22 09:29:33 +0300

    final Firepower

--- Project_IDF_Operation_First_Strike/Firepower/Drone.cs
+++ Project_IDF_Operation_First_Strike/Firepower/Drone.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+class Drone: StrikeWeapon
+{
+    public Drone() : base("Drone",3 , "people, vehicles")
+    {
+    }
+    public override void Strike(int amount)
+    {
+        if (amount > 1)
+        {
+            Console.WriteLine("can not strike more then  once at the same time");
+        }
+        if (amount > this.AmmunitionCapacity )
+        {
+            Console.WriteLine("you have no  Ammunition to complete this strike");
+        }
+        
+        else
+        {
+            Console.WriteLine($"{nameOfWeapon} strikes a {EffectiveAgainst}  target {amount} of times!");
+            this.AmmunitionCapacity -= amount;
+        }
+    }
+}
--- Project_IDF_Operation_First_Strike/Firepower/StrikeWeapon.cs
+++ Project_IDF_Operation_First_Strike/Firepower/StrikeWeapon.cs
@@ -1,1 +1,21 @@
-﻿
+﻿ abstract class StrikeWeapon
+{
+    public string nameOfWeapon;
+    public int AmmunitionCapacity;
+    public double FuelSupply;
+    public string EffectiveAgainst;
+
+    public StrikeWeapon(string nameOfweapon,int AmmunitionCapacity,string EffectiveAgainst)
+    {
+        this.nameOfWeapon = nameOfWeapon;
+        this.AmmunitionCapacity = AmmunitionCapacity;
+        
+        this.EffectiveAgainst = EffectiveAgainst;
+        
+
+
+
+    }
+    abstract public void Strike(int amount);
+    
+}
