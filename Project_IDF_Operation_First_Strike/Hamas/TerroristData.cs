0292bd1b353b22ae4ad80a0d4e7b10507dbd60ce
dov zucker <ydovzucker@gmail.com>
2025-05-22 15:17:58 +0300

    general

---
+++ Project_IDF_Operation_First_Strike/Hamas/TerroristData.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+public class TerroristData
+{
+    //name
+    private string _name;
+    //Rank
+    private int _rank;
+    //Status = Bool
+    private bool _status;
+    //Weapons
+    private string _weapons;
+    public TerroristData(string name, int rank, bool status, string weapons)
+    {
+        this._name = name;
+        this._rank = rank;
+        this._status = status;
+        this._weapons = weapons;
+    }
+
+        //Method for get or set a name.
+        public void setname(string namei)
+        {
+            this._name = namei;
+        }
+        public string getname()
+        { 
+            return this._name; 
+        }
+
+        //Method for get or set a rank.
+        public void setrank(int ranki)
+        {
+            this._rank = ranki;
+        }
+        public int getrank(int ranki)
+        { 
+            return this._rank;
+        }
+
+        //Method for get or set a status.
+        public void setstatus(bool statusi)
+        { this._status = statusi; }
+        public bool getstatus(bool statusi)
+        { return this._status; }
+
+        //Method for get or set a weapons.
+        public void setWeapons(string weaponsi)
+        { this._weapons = weaponsi; }
+        public string getWeapons(string weaponsi)
+        { return this._weapons; }
+
+}
+
--- Project_IDF_Operation_First_Strike/Program.cs
+++ Project_IDF_Operation_First_Strike/Program.cs
@@ -6,12 +6,25 @@ using System.Windows.Forms;
 
 namespace Project_IDF_Operation_First_Strike
 {
-    internal static class Program
+    internal static class Program 
     {
- 
+
         static void Main()
         {
             Console.WriteLine("Test");
+            Dictionary<string, TerroristData> List_TR = new Dictionary<string, TerroristData>();
+            TerroristData T1 = new TerroristData("Mohamad",9,true,"M16");
+            TerroristData T2 = new TerroristData("Ali", 6, true, "kalatch");
+            TerroristData T3 = new TerroristData("Abo Raed", 1, true, "kalatch");
+            List_TR.Add("Mohamaad",T1);
+            List_TR.Add("Ali",T2);
+            List_TR.Add("Abo Raed",T3);
+            foreach (var kvp in List_TR)
+            {
+                Console.WriteLine(kvp.Key + ": " +kvp.Value.getname());
+            }
+
+
         }
     }
 }
