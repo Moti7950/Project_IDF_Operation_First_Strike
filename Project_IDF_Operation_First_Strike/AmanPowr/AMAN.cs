492b6cf0a24558317d2748f11457e5a27e6e3ecf
Moti <112508741+Moti7950@users.noreply.github.com>
2025-05-22 14:49:03 +0300

    Update Folder

---
+++ Project_IDF_Operation_First_Strike/AmanPowr/AMAN.cs
@@ -0,0 +1,12 @@
+﻿class AMAN
+{
+    string name;
+    string Locaition;
+    string TimeStamp_Info;
+    public void NameOfTarget(string name)
+    { }
+
+    public void LocaitionOfTarget(string Locaition, string TimeStamp_Info)
+    { 
+    }
+}
