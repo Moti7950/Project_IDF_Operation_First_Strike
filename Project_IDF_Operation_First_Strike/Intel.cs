0c0c3fe41fb086bfa3307daa0631152b8c54f065
dov zucker <ydovzucker@gmail.com>
2025-05-25 18:15:33 +0300

    added strike manager

--- Project_IDF_Operation_First_Strike/Intel.cs
+++ Project_IDF_Operation_First_Strike/Intel.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class Intel
+{
+    public TerroristData Terrorist { get; set; }
+    public string Location { get; set; }
+    public DateTime Timestamp { get; set; }
+    public static List<Intel> listOfIntel = new List<Intel>();
+
+    public Intel(TerroristData terrorist, string location, DateTime timestamp)
+    {
+        this.Terrorist = terrorist;
+        this.Location = location;
+        this.Timestamp = timestamp;
+
+        listOfIntel.Add(this);
+    }
+    public void PrintReport()
+    {
+        Console.WriteLine($"Terrorist: {Terrorist.getName()}"); 
+        Console.WriteLine($"Location: {Location}");
+        Console.WriteLine($"Timestamp: {Timestamp:yyyy-MM-dd HH:mm:ss}");
+
+    }
+
+}
